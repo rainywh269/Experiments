115c58fcc8fedafef718c77bf0a74288fcf1668b
WooParadog <guohaochuan@gmail.com>
2012-03-19 10:54:23 +0800

    add

--- go/main.go
+++ go/main.go
@@ -1,7 +1,52 @@
 package main
 
-import fmt "fmt" // Package implementing formatted I/O.
+import (
+    "fmt"
+    "blackfriday"
+)
+type Author struct{
+    id int
+    name string
+}
+type Post struct{
+    id int
+    author Author
+    title, body string
+}
+type Note struct{
+    Post
+    img string
+}
 
-func main() {
-        fmt.Printf("Hello, world; or Καλημέρα κόσμε; or こんにちは 世界\n")
+type A struct {
+    ax, ay int
+}
+type B struct {
+    A
+    bx, by float64
+}
+type C interface{
+    author()
+}
+
+func main(){
+    var author=Author{id:1,name:"wooparadog"}
+
+    var d = Note{
+        Post:Post{
+            id:1,
+            author:author,
+            title:"titlepa",
+            body:"body",
+        }, 
+        img:"img" }
+
+    d.author.name = "testAuthor"
+
+    fmt.Println(d.title)
+    var ai C
+    ai = d.author
+}
+func (p Author) author(){
+    fmt.Print("author:" + p.name)
 }
