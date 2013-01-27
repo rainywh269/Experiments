8c900dcb003d8029d409201c26a4a1830d1338ec
WooParadog <guohaochuan@gmail.com>
2013-01-27 17:18:42 +0800

    add fixtures

--- for_her/spider2/main.py
+++ for_her/spider2/main.py
@@ -8,9 +8,12 @@
 
 import os
 import re
+from collections import defaultdict
 import requests
 import cPickle
 import hashlib
+from lxml import etree
+
 upload_url = "http://knowminer.at:8080/team-pdf-extraction-webapp/extract/?action=upload"
 result_extractor = re.compile("<h\d>(.*)<div", re.S|re.M)
 block_splitter = re.compile("<h\d>")
@@ -54,7 +57,7 @@ def cache(content_getter):
 def upload(pdf_file):
     try:
         files = {
-                "file": open(pdf_file)
+                "file": open(pdf_file, 'rb')
                 }
     except IOError:
         print "file io error"
@@ -69,35 +72,65 @@ def ul_to_list(html):
     'asdfas asd fas df '
     """
     return list_item_finder.findall(html) or html
-    
+
 
 def parse_result(content):
+    """
+    metas:
+    {
+        "key": content # content is str if singular, else it's a list
+        "author": ['rainywh269', 'wooparadog'],
+        'abstract': "simple extractor",
+    }
+
+    blocks:
+    {
+        (name, chance): ["block1", "block2"...]
+    }
+    """
     html = content
     content = result_extractor.findall(html)
-    result = {}
+    metas = {}
+    block_dict = defaultdict(list)
     if content:
         content = content[0]
         for block in block_splitter.split(content):
             meta = block_end_splitter.split(block, 1)
             k, v = meta
-            result[k.strip()] = ul_to_list(v.strip())
+            metas[k.strip()] = ul_to_list(v.strip())
 
-    return result
+    tree = etree.HTML(html.encode('utf8'))
+    blocks = tree.xpath('//div[@id="preview"]/div[contains(@class, "block")]')
+    for block in blocks:
+        title = block.xpath(".//@title")[0]
+        title, chances = map(str.strip, title.split("("))
+        chances = chances.split("%", 1)[0]
+        content = block.xpath(".//span/text()")
+        block_dict[(title, chances)].append(content)
+
+    return metas, block_dict
 
 def analysis_pdf(pdf_file):
     content = upload(pdf_file)
     result = parse_result(content)
     return result
 
-
 def main():
-    test_pdf_file_path = "/home/wooparadog/Dropbox/books/Papers/PDF4test/S10-8.pdf"
-    result = analysis_pdf(test_pdf_file_path)
-    for k, v in result.iteritems():
+    test_pdf_file_path = "/home/wooparadog/Dropbox/books/Papers/PDF4test/S19-5.pdf"
+    metas, blocks = analysis_pdf(test_pdf_file_path)
+    for k, v in metas.iteritems():
         print "++++++++++++++"
         print k
         print "--------------"
         print v
+
+    for k,v in blocks.iteritems():
+        print "++++++++++++++"
+        print k
+        print "--------------"
+        print v
+
+
 
 if __name__ == '__main__':
     #import doctest
