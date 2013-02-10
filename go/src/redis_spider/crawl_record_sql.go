963009221284c278e677f5d5d5fe53e9c171acce
WooParadog <guohaochuan@gmail.com>
2013-02-10 23:28:20 +0800

    implement sqlite recorder; TODO: choose recorder at config

--- go/src/redis_spider/crawl_record_sql.go
+++ go/src/redis_spider/crawl_record_sql.go
@@ -7,34 +7,41 @@ import(
     _ "github.com/mattn/go-sqlite3"
 )
 
+type sqlite_row struct{
+    url string
+}
+
 type SqliteRecorder struct{
     conn *sql.DB
     mutex sync.Mutex
 }
 
-func CreateSqliteRecorder(network, location string) SqliteRecorder{
-    conn, err := sql.Open("sqlite3", SQLITE_FILE)
+func CreateSqliteRecorder(location string) SqliteRecorder{
+    conn, err := sql.Open("sqlite3", location)
     if err != nil {
         log.Fatal("Sqlite connection fail!\nFile: ", SQLITE_FILE)
     }
-    _, err = conn.Exec("create table download_record if not exists(url varchar(512) primary key) ")
+    _, err = conn.Exec("create table if not exists download_record(url varchar(512) primary key) ")
     if err != nil{
         log.Fatal("Sqlite Recorder Table download_record does not exists and cannot be created")
     }
-    log.Println("Sqlite Recorder Connected")
+    log.Println("Sqlite Recorder Connected : ", location)
     return SqliteRecorder{conn: conn}
 }
 
-
 func (self *SqliteRecorder) MarkAsFinished(url string){
     self.mutex.Lock()
+    self.conn.Exec("insert into download_record values(?)", url)
     self.mutex.Unlock()
 }
 
 func (self *SqliteRecorder) HasFinished(url string) bool{
     self.mutex.Lock()
-    row := self.conn.QueryRow("select url from download_record where url =%s", url)
+    row := self.conn.QueryRow("select url from download_record where url=?", url)
     self.mutex.Unlock()
-    log.Fatal(">>>>>", row)
+    sqlite_record := sqlite_row{}
+    if err:=row.Scan(&sqlite_record.url); err!=nil{
+        return false
+    }
     return true
 }
--- go/src/redis_spider/main.go
+++ go/src/redis_spider/main.go
@@ -7,7 +7,8 @@ var worker_pool chan *Downloader
 var exit_signal chan int
 
 func main(){
-    recorder := CreateRedisRecord(REDIS_NETWORK, REDIS_LOCATION)
+    recorder := CreateSqliteRecorder(SQLITE_FILE)
+    //recorder := CreateRedisRecord(REDIS_NETWORK, REDIS_LOCATION)
     tumblr_sources := parse_tumblr_sources()
     worker_pool = MakeDownloaderWorkers(DirectDownloaderFactory)
     for _, tumblr_source := range tumblr_sources{
