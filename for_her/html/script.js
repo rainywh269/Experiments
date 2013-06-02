66f1da842f9e906af6e64d985bdb14b3274d9e1d
WooParadog <guohaochuan@gmail.com>
2013-06-02 21:10:25 +0800

    update

--- for_her/html/script.js
+++ for_her/html/script.js
@@ -6,19 +6,43 @@
  */
 
 $(function(){
+    $(document).click(function(e){
+        $("#post-form").hide();
+    });
+    $(".content-hd").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        var $this=$(this), section=$this.parents("li.sub-section"),
+            section_id=section.data('section-id'), post_form=$("#post-form"),
+            pos = $this.offset();
+        if(post_form.data('section-id') != section_id){
+            post_form.find("textarea").val("");
+        }
+        post_form.data("section-id", section_id);
+        post_form.css({
+            left: pos.left,
+            top: pos.top + 30
+        });
+        post_form.show();
+    });
+
+    $("form").click(function(e){
+        e.stopPropagation();
+    });
+
     $("form").submit(function(e){
         e.preventDefault();
-        var $this=$(this), section=$this.parents("li.sub-section"),
-            section_id=section.data('section-id'),
-            text=$this.find("input[name=text]").val();
+        var $this=$(this), section_id=$this.data("section-id"),
+            text=$this.find("textarea[name=text]").val();
 
         $.post(
-            "/form/url",
+            "/",
             {
-                text: text
+                text: text, 
+                section_id: section_id
             },
             function(data){
-                alert(1);
+                console.log(section_id+text);
             });
     });
 });
